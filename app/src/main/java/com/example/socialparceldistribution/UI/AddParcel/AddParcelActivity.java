c5c5735355c982d822259ea899731c78507b9385
ShemuelUriyaAschkenazy <uriyaas@gmail.com>
2019-12-09 16:07:52 +0200

    layout for addParcelActivity- messenger layout gone and appear

--- app/src/main/java/com/example/socialparceldistribution/UI/AddParcel/AddParcelActivity.java
+++ app/src/main/java/com/example/socialparceldistribution/UI/AddParcel/AddParcelActivity.java
@@ -5,6 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Switch;
 
 import com.example.socialparceldistribution.R;
 
@@ -13,5 +20,19 @@ public class AddParcelActivity extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_parcel_layout);
+
+        final LinearLayout messengerDetailsLO = findViewById(R.id.lo_messenger_details);
+        messengerDetailsLO.setVisibility(View.GONE);
+
+        Switch pickUpSuggested = findViewById(R.id.swPickUp);
+        pickUpSuggested.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked)
+                    messengerDetailsLO.setVisibility(View.VISIBLE);
+                else
+                    messengerDetailsLO.setVisibility(View.GONE);
+            }
+        });
     }
 }
