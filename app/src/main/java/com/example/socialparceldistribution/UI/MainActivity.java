cbe67fbf70ac3eb04abf47bb9bb452f8e278ee74
ShemuelUriyaAschkenazy <uriyaas@gmail.com>
2019-12-09 13:56:18 +0200

    layout for addParcelActivity

--- app/src/main/java/com/example/socialparceldistribution/UI/MainActivity.java
+++ app/src/main/java/com/example/socialparceldistribution/UI/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.socialparceldistribution.UI;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.socialparceldistribution.R;
+import com.example.socialparceldistribution.UI.AddParcel.AddParcelActivity;
 
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button addParcel= findViewById(R.id.btn_addParcel);
+        addParcel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(MainActivity.this, AddParcelActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
 
