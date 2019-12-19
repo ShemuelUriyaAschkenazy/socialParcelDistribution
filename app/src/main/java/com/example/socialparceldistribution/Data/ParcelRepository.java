fc0034e273d35fa7a1dffce39565f8bbd2863c15
יעקב ברין <ybrin@g.jct.ac.il>
2019-12-19 21:07:54 +0200

    commit02

--- app/src/main/java/com/example/socialparceldistribution/Data/ParcelRepository.java
+++ app/src/main/java/com/example/socialparceldistribution/Data/ParcelRepository.java
@@ -1,4 +1,20 @@
 package com.example.socialparceldistribution.Data;
 
+import androidx.lifecycle.MutableLiveData;
+
 public class ParcelRepository {
+    ParcelDataSource parcelDataSource;
+    private ParcelRepository(){}
+
+    private static ParcelRepository instance;
+    public static ParcelRepository getInstance() {
+        if (instance == null)
+            instance = new ParcelRepository();
+        return instance;
+    }
+
+    public MutableLiveData<Boolean> getStatus(){
+        //todo
+        return new MutableLiveData<Boolean>();
+    }
 }
--- app/src/main/java/com/example/socialparceldistribution/UI/AddParcel/AddParcelViewModel.java
+++ app/src/main/java/com/example/socialparceldistribution/UI/AddParcel/AddParcelViewModel.java
@@ -1,10 +1,30 @@
 package com.example.socialparceldistribution.UI.AddParcel;
 
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.example.socialparceldistribution.Data.ParcelRepository;
+import com.example.socialparceldistribution.Entities.Parcel;
 
-public class AddParcelViewModel extends ViewModel {
+public class AddParcelViewModel extends AndroidViewModel {
     ParcelRepository parcelRepository;
+    MutableLiveData<Boolean> booleanMutableLiveData;
+
+    public AddParcelViewModel(@NonNull Application application) {
+        super(application);
+        parcelRepository = ParcelRepository.getInstance();
+        booleanMutableLiveData = new MutableLiveData<>();
+        //booleanMutableLiveData.postValue(true);
+    }
+
+
+    public void addParcel(Parcel parcel){
+        parcelRepository.addParcel();
+    }
+
     
 }
