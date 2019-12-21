56996e2884b4f366fc0deae79c379fce03f3f1b4
ShemuelUriyaAschkenazy <uriyaas@gmail.com>
2019-12-21 19:41:09 +0200

    עבודה מול firebase

--- app/src/main/java/com/example/socialparceldistribution/Data/ParcelRepository.java
+++ app/src/main/java/com/example/socialparceldistribution/Data/ParcelRepository.java
@@ -2,15 +2,20 @@ package com.example.socialparceldistribution.Data;
 
 import androidx.lifecycle.MutableLiveData;
 
+import com.example.socialparceldistribution.Entities.Parcel;
+
+import java.util.List;
+
 public class ParcelRepository {
 
+    MutableLiveData<List<Parcel>> mutableLiveData;
     ParcelDataSource parcelDataSource= new ParcelDataSource();
 
     private ParcelRepository(){
         ParcelDataSource.changedListener changedListener= new ParcelDataSource.changedListener() {
             @Override
             public void change() {
-                //TODO parcelDataSource.getParcelsList();
+                //todo(parcelDataSource.getParcelsList());
             }
         };
         parcelDataSource.setChangedListener(changedListener);
