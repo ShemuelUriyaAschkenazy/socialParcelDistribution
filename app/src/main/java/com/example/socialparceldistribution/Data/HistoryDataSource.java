3a333bf7646e08c119430503d89793b3d3934ec7
ShemuelUriyaAschkenazy <uriyaas@gmail.com>
2020-01-03 01:19:22 +0200

    room

--- app/src/main/java/com/example/socialparceldistribution/Data/HistoryDataSource.java
+++ app/src/main/java/com/example/socialparceldistribution/Data/HistoryDataSource.java
@@ -1,4 +1,22 @@
 package com.example.socialparceldistribution.Data;
 
-public class HistoryDataSource {
+import android.content.Context;
+
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import com.example.socialparceldistribution.Entities.Parcel;
+
+public abstract class HistoryDataSource extends RoomDatabase {
+
+    public static final String DATABASE_NAME="database.db";
+    public static HistoryDataSource database;
+
+    public static HistoryDataSource getInstance(Context context){
+        if (database==null)
+            database= Room.databaseBuilder(context,HistoryDataSource.class,DATABASE_NAME).allowMainThreadQueries().build();
+        return database;
+    }
+
+    public abstract ParcelDao getParcelDao();
 }
