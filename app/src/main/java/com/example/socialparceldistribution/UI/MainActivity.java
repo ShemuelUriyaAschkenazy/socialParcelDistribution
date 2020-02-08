4a090a279df91c0a8b2491497c827e6058da1058
יעקב ברין <ybrin@g.jct.ac.il>
2020-02-08 22:44:12 +0200

    animation-list

--- app/src/main/java/com/example/socialparceldistribution/UI/MainActivity.java
+++ app/src/main/java/com/example/socialparceldistribution/UI/MainActivity.java
@@ -1,16 +1,15 @@
 package com.example.socialparceldistribution.UI;
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
-import com.example.socialparceldistribution.Data.ParcelDataSource;
 import com.example.socialparceldistribution.R;
 import com.example.socialparceldistribution.UI.AddParcel.AddParcelActivity;
 import com.example.socialparceldistribution.UI.ParcelHistory.HistoryParcelsActivity;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends Activity {
     @Override
@@ -40,6 +39,9 @@ public class MainActivity extends Activity {
             }
         });
 
+        ImageView imageView = findViewById(R.id.parcels_animation);
+        AnimationDrawable animationDrawable = (AnimationDrawable)imageView.getDrawable();
+        animationDrawable.start();
     }
 
     @Override
