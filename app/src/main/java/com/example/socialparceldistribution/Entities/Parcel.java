4857ccfc42c72e02832d67781491c4688f41c5c0
ShemuelUriyaAschkenazy <uriyaas@gmail.com>
2020-02-09 02:18:11 +0200

    תיקונים בעיקר קוסמטיים  במחלקת parcel

--- app/src/main/java/com/example/socialparceldistribution/Entities/Parcel.java
+++ app/src/main/java/com/example/socialparceldistribution/Entities/Parcel.java
@@ -5,7 +5,6 @@ import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 import androidx.room.TypeConverter;
 import androidx.room.TypeConverters;
-
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -15,11 +14,158 @@ import java.util.Map;
 @Entity
 public class Parcel {
 
+    @TypeConverters(DateConverter.class)
+    private Date deliveryDate;
+    @TypeConverters(DateConverter.class)
+    private Date arrivalDate;
+    private String recipientPhone;
+    private String recipientEmail;
+    @NonNull
+    @PrimaryKey
+    private String parcelId = "id";
+    @TypeConverters(ParcelType.class)
+    private ParcelType parcelType;
+    @TypeConverters(ParcelStatus.class)
+    private ParcelStatus parcelStatus;
+    private Boolean isFragile;
+    private Double weight;
+    private String warehouseAddress;
+    private String recipientAddress;
+    private UserLocation recipientUserLocation;
+    private HashMap<String, Boolean> messengers;
+    @TypeConverters(UserLocationConverter.class)
+    private UserLocation warehouseUserLocation;
+    private String recipientName;
 
     public Parcel() {
     }
 
+    public Parcel(ParcelType parcelType, ParcelStatus parcelStatus, Boolean isFragile, Double weight, UserLocation warehouseUserLocation, String recipientName, String warehouseAddress, String recipientAddress, UserLocation recipientUserLocation, Date deliveryDate, Date arrivalDate, String recipientPhone, String recipientEmail, HashMap<String, Boolean> messengers) {
+        this.parcelType = parcelType;
+        this.parcelStatus = parcelStatus;
+        this.isFragile = isFragile;
+        this.weight = weight;
+        this.warehouseUserLocation = warehouseUserLocation;
+        this.recipientName = recipientName;
+        this.warehouseAddress = warehouseAddress;
+        this.recipientAddress = recipientAddress;
+        this.recipientUserLocation = recipientUserLocation;
+        this.deliveryDate = deliveryDate;
+        this.arrivalDate = arrivalDate;
+        this.recipientPhone = recipientPhone;
+        this.recipientEmail = recipientEmail;
+        this.messengers = messengers;
+    }
 
+    public enum ParcelType {
+        envelope(0), smallPackage(1), bigPackage(2);
+        private final Integer code;
+        ParcelType(Integer value) {
+            this.code = value;
+        }
+        public Integer getCode() {
+            return code;
+        }
+        @TypeConverter
+        public static ParcelType getType(Integer numeral) {
+            for (ParcelType ds : values())
+                if (ds.code.equals(numeral))
+                    return ds;
+            return null;
+        }
+        @TypeConverter
+        public static Integer getTypeInt(ParcelType parcelType) {
+            if (parcelType != null)
+                return parcelType.code;
+            return null;
+        }
+    }
+
+    public enum ParcelStatus {
+        registered(0), pickUpSuggested(1), onTheWay(2), successfullyArrived(3);
+        private final Integer code;
+        ParcelStatus(Integer value) {
+            this.code = value;
+        }
+        public Integer getCode() {
+            return code;
+        }
+        @TypeConverter
+        public static ParcelStatus getStatus(Integer numeral) {
+            for (ParcelStatus ds : values())
+                if (ds.code == numeral)
+                    return ds;
+            return null;
+        }
+        @TypeConverter
+        public static Integer getTypeInt(ParcelStatus parcelStatus) {
+            if (parcelStatus != null)
+                return parcelStatus.code;
+            return null;
+        }
+    }
+
+    //region converters
+    public static class DateConverter {
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+
+        @TypeConverter
+        public Date fromTimestamp(String date) throws ParseException {
+            return (date == null ? null : format.parse(date));
+        }
+
+        @TypeConverter
+        public String dateToTimestamp(Date date) {
+            return date == null ? null : format.format(date);
+        }
+    }
+
+    public static class MessengersConverter {
+        @TypeConverter
+        public HashMap<String, Boolean> fromString(String value) {
+            if (value == null || value.isEmpty())
+                return null;
+            String[] mapString = value.split(","); //split map into array of (string,boolean) strings
+            HashMap<String, Boolean> hashMap = new HashMap<>();
+            for (String s1 : mapString) //for all (string,boolean) in the map string
+            {
+                if (!s1.isEmpty()) {//is empty maybe will needed because the last char in the string is ","
+                    String[] s2 = s1.split(":"); //split (string,boolean) to person string and boolean string.
+                    Boolean aBoolean = Boolean.parseBoolean(s2[1]);
+                    hashMap.put(/*person string:*/s2[0], aBoolean);
+                }
+            }
+            return hashMap;
+        }
+
+        @TypeConverter
+        public String asString(HashMap<String, Boolean> map) {
+            if (map == null)
+                return null;
+            StringBuilder mapString = new StringBuilder();
+            for (Map.Entry<String, Boolean> entry : map.entrySet())
+                mapString.append(entry.getKey()).append(":").append(entry.getValue()).append(",");
+            return mapString.toString();
+        }
+    }
+
+    public static class UserLocationConverter {
+        @TypeConverter
+        public UserLocation fromString(String value) {
+            if (value == null || value.equals(""))
+                return null;
+            double lat = Double.parseDouble(value.split(" ")[0]);
+            double lang = Double.parseDouble(value.split(" ")[1]);
+            return new UserLocation(lat, lang);
+        }
+
+        @TypeConverter
+        public String asString(UserLocation warehouseUserLocation) {
+            return warehouseUserLocation == null ? "" : warehouseUserLocation.getLat() + " " + warehouseUserLocation.getLon();
+        }
+    }
+    //endregion
+//region getters/setters
     @NonNull
     public String getParcelId() {
         return parcelId;
@@ -62,7 +208,6 @@ public class Parcel {
         this.recipientName = recipientName;
     }
 
-
     public Date getDeliveryDate() {
         return deliveryDate;
     }
@@ -95,67 +240,6 @@ public class Parcel {
         this.recipientEmail = recipientEmail;
     }
 
-    public enum ParcelType {
-        envelope(0), smallPackage(1), bigPackage(2);
-        private final Integer code;
-
-        ParcelType(Integer value) {
-            this.code = value;
-        }
-
-        public Integer getCode() {
-            return code;
-        }
-
-        @TypeConverter
-        public static ParcelType getType(Integer numeral) {
-            for (ParcelType ds : values()) {
-                if (ds.code.equals(numeral)) {
-                    return ds;
-                }
-            }
-            return null;
-        }
-
-        @TypeConverter
-        public static Integer getTypeInt(ParcelType parcelType) {
-            if (parcelType != null)
-                return parcelType.code;
-            return null;
-        }
-    }
-
-    public enum ParcelStatus {
-        registered(0), pickUpSuggested(1), onTheWay(2), successfullyArrived(3);
-        private final Integer code;
-
-        ParcelStatus(Integer value) {
-            this.code = value;
-        }
-
-        public Integer getCode() {
-            return code;
-        }
-
-        @TypeConverter
-        public static ParcelStatus getStatus(Integer numeral) {
-            for (ParcelStatus ds : values()) {
-                if (ds.code == numeral) {
-                    return ds;
-                }
-            }
-            return null;
-        }
-
-        @TypeConverter
-        public static Integer getTypeInt(ParcelStatus parcelStatus) {
-            if (parcelStatus != null)
-                return parcelStatus.code;
-            return null;
-        }
-    }
-
-
     public ParcelStatus getParcelStatus() {
         return parcelStatus;
     }
@@ -164,18 +248,6 @@ public class Parcel {
         this.parcelStatus = parcelStatus;
     }
 
-    @NonNull
-    @PrimaryKey
-    private String parcelId = "id";
-    @TypeConverters(ParcelType.class)
-    private ParcelType parcelType;
-    @TypeConverters(ParcelStatus.class)
-
-    private ParcelStatus parcelStatus;
-    private Boolean isFragile;
-
-
-    private Double weight;
     public UserLocation getWarehouseUserLocation() {
         return warehouseUserLocation;
     }
@@ -183,13 +255,6 @@ public class Parcel {
     public void setWarehouseUserLocation(UserLocation warehouseUserLocation) {
         this.warehouseUserLocation = warehouseUserLocation;
     }
-
-    @TypeConverters(UserLocationConverter.class)
-    private UserLocation warehouseUserLocation;
-
-
-    private String recipientName;
-
 
     public String getRecipientAddress() {
         return recipientAddress;
@@ -215,34 +280,6 @@ public class Parcel {
         this.warehouseAddress = warehouseAddress;
     }
 
-    private String warehouseAddress;
-    private String recipientAddress;
-    private UserLocation recipientUserLocation;
-
-    public Parcel(ParcelType parcelType, ParcelStatus parcelStatus, Boolean isFragile, Double weight, UserLocation warehouseUserLocation, String recipientName, String warehouseAddress, String recipientAddress, UserLocation recipientUserLocation, Date deliveryDate, Date arrivalDate, String recipientPhone, String recipientEmail, HashMap<String, Boolean> messengers) {
-        this.parcelType = parcelType;
-        this.parcelStatus = parcelStatus;
-        this.isFragile = isFragile;
-        this.weight = weight;
-        this.warehouseUserLocation = warehouseUserLocation;
-        this.recipientName = recipientName;
-        this.warehouseAddress = warehouseAddress;
-        this.recipientAddress = recipientAddress;
-        this.recipientUserLocation = recipientUserLocation;
-        this.deliveryDate = deliveryDate;
-        this.arrivalDate = arrivalDate;
-        setRecipientPhone(recipientPhone);
-        this.recipientEmail = recipientEmail;
-        this.messengers = messengers;
-    }
-
-    @TypeConverters(DateConverter.class)
-    private Date deliveryDate;
-    @TypeConverters(DateConverter.class)
-    private Date arrivalDate;
-    private String recipientPhone;
-    private String recipientEmail;
-
     public HashMap<String, Boolean> getMessengers() {
         return messengers;
     }
@@ -250,74 +287,5 @@ public class Parcel {
     public void setMessengers(HashMap<String, Boolean> messengers) {
         this.messengers = messengers;
     }
-
-    private HashMap<String, Boolean> messengers;
-
-    public static class DateConverter {
-        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
-        @TypeConverter
-        public Date fromTimestamp(String date) throws ParseException {
-            return (date == null ? null : format.parse ( date ));
-        }
-
-        @TypeConverter
-        public String dateToTimestamp(Date date) {
-            return date == null ? null : format.format(date);
-        }
-    }
-
-    public static class MessengersConverter {
-        @TypeConverter
-        public HashMap<String, Boolean> fromString(String value) {
-            if (value==null||value.isEmpty())
-                return null;
-            String[] mapString = value.split(","); //split map into array of (string,boolean) strings
-            HashMap<String, Boolean>  hashMap= new HashMap<>();
-            for (String s1:mapString) //for all (string,boolean) in the map string
-            {
-                if(!s1.isEmpty()){//is empty maybe will needed because the last char in the string is ","
-                String[] s2= s1.split(":"); //split (string,boolean) to person string and boolean string.
-                Boolean aBoolean= Boolean.parseBoolean(s2[1]);
-                hashMap.put(/*person string:*/s2[0],aBoolean);}
-            }
-            return hashMap;
-        }
-
-        @TypeConverter
-        public String asString(HashMap<String, Boolean> map) {
-            if (map==null)
-                return null;
-            StringBuilder mapString= new StringBuilder();
-            for (Map.Entry<String,Boolean> entry:map.entrySet())
-            {
-                mapString.append(entry.getKey()+":"+((entry.getValue()))+",");
-            }
-            return mapString.toString();
-        }
-
-
-
-    }
-
-    public static class UserLocationConverter {
-        @TypeConverter
-        public UserLocation fromString(String value) {
-            if (value==null||value.equals(""))
-                return null;
-            Double lat= Double.parseDouble(value.split(" ")[0]);
-            Double lang = Double.parseDouble(value.split(" ")[1]);
-            return new UserLocation(lat, lang);
-        }
-
-        @TypeConverter
-        public String asString(UserLocation warehouseUserLocation) {
-            return warehouseUserLocation==null? "":warehouseUserLocation.getLat() + " " + warehouseUserLocation.getLon();
-        }
-
-
-
-    }
-
-
-
+//endregion
 }
